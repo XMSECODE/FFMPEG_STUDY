fe04837dfcf3ef9e1b3f1acab1fe30e25835cd20
xms <714350089@qq.com>
2017-06-15 21:35:10 +0800

    创建模板文件

---
+++ FFMPEG_STUDY/FFMPEG_STUDY/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFMPEG_STUDY
+//
+//  Created by xiangmingsheng on 2017/6/15.
+//  Copyright © 2017年 XMSECODE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FFMPEG_STUDY/FFMPEG_STUDY/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FFMPEG_STUDY
+//
+//  Created by xiangmingsheng on 2017/6/15.
+//  Copyright © 2017年 XMSECODE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
